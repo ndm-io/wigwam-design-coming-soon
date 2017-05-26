3ad5c9f55e530528cb67c89ae5265623d5195cc1
Rob Atherton <robather@googlemail.com>
2017-05-26 14:03:21 +0100

    added twilio version to test envs

--- app.js
+++ app.js
@@ -78,7 +78,10 @@ app.use(function (error, req, res) {
 
 
 http.listen(app.get('port'), function () {
-    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+    console.log('Express server listening on port %d in %s mode with twilio %s',
+        app.get('port'),
+        app.get('env'),
+        process.env.TWILIO_VERSION);
 });
 
 module.exports = app;
