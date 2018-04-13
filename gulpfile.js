fdddc7439008afdc2aa6732680183fd2cec3c55a
Rob Atherton <robather@googlemail.com>
2018-04-13 08:02:10 +0100

    Optimised images

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,60 @@
-var gulp = require('gulp'),
-    _ = require('lodash');
-homePaths = require('./gulp/homeConstants').paths;
+const gulp = require("gulp");
+const _ = require("lodash");
+const homePaths = require("./gulp/homeConstants").paths;
 
 /**
  * Home Site Tasks
  */
 
-var homeTaskScripts = require('./gulp/home/taskScripts'),
-    homeTaskHtml = require('./gulp/home/taskHtml'),
-    homeTaskCss = require('./gulp/home/taskCss'),
-    homeTaskImg = require('./gulp/home/taskImg'),
-    homeTaskAssets = require('./gulp/home/taskAssets'),
-    homeTaskBrowserify = require('./gulp/home/taskBrowserify'),
-    homeTaskTranslation = require('./gulp/home/taskTranslation'),
-    homeTaskSitemap = require('./gulp/home/taskSitemap'),
-    homeTaskFavicons = require('./gulp/home/taskFavicons');
+var homeTaskScripts = require("./gulp/home/taskScripts"),
+    homeTaskHtml = require("./gulp/home/taskHtml"),
+    homeTaskCss = require("./gulp/home/taskCss"),
+    homeTaskImg = require("./gulp/home/taskImg"),
+    homeTaskAssets = require("./gulp/home/taskAssets"),
+    homeTaskBrowserify = require("./gulp/home/taskBrowserify"),
+    homeTaskTranslation = require("./gulp/home/taskTranslation"),
+    homeTaskSitemap = require("./gulp/home/taskSitemap"),
+    homeTaskFavicons = require("./gulp/home/taskFavicons");
 
-gulp.task('scripts', homeTaskScripts);
-gulp.task('browserify', homeTaskBrowserify);
-gulp.task('translation', homeTaskTranslation);
-gulp.task('html', homeTaskHtml);
-gulp.task('css', homeTaskCss);
-gulp.task('img', homeTaskImg);
-gulp.task('assets', homeTaskAssets);
-gulp.task('sitemap', homeTaskSitemap);
-gulp.task('favicons', homeTaskFavicons);
+gulp.task("scripts", homeTaskScripts);
+gulp.task("browserify", homeTaskBrowserify);
+gulp.task("translation", homeTaskTranslation);
+gulp.task("html", homeTaskHtml);
+gulp.task("css", homeTaskCss);
+gulp.task("img", homeTaskImg);
+gulp.task("assets", homeTaskAssets);
+gulp.task("sitemap", homeTaskSitemap);
+gulp.task("favicons", homeTaskFavicons);
 
 /**
  * Watch Tasks
  */
 
-gulp.task('watch', function () {
-    gulp.watch(homePaths.browserify.watch, ['browserify']);
-    gulp.watch(homePaths.translation.watch, ['translation']);
-    gulp.watch(homePaths.html.src, ['html']);
-    gulp.watch(homePaths.css.src, ['css']);
-    gulp.watch(homePaths.img.src, ['img']);
-    gulp.watch(homePaths.assets.src, ['assets']);
+gulp.task("watch", function () {
+    gulp.watch(homePaths.browserify.watch, ["browserify"]);
+    gulp.watch(homePaths.translation.watch, ["translation"]);
+    gulp.watch(homePaths.html.src, ["html"]);
+    gulp.watch(homePaths.css.src, ["css"]);
+    gulp.watch(homePaths.img.src, ["img"]);
+    gulp.watch(homePaths.assets.src, ["assets"]);
 });
 
 
 var build = [
-    'browserify',
-    'translation',
-    'scripts',
-    'html',
-    'css',
-    'assets',
-    'img',
-    'sitemap',
-    'favicons'
+    "browserify",
+    "translation",
+    "scripts",
+    "html",
+    "css",
+    "assets",
+    "img",
+    "sitemap",
+    "favicons"
 ];
 
 var defaults = _.clone(build);
-defaults.push('watch');
+defaults.push("watch");
 
 
-gulp.task('build', build);
-gulp.task('default', defaults);
+gulp.task("build", build);
+gulp.task("default", defaults);
